42fe2a5211b0e6069405509f5f38136e044e4142
Sheriff Abdulraheem <sheriffabdulraheemafunsho23@gmail.com>
2025-10-15 17:45:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7284709572-3d188",
+  "appId": "1:513286659639:web:8354b387471c5c8ba0a9b2",
+  "apiKey": "AIzaSyDtJLqfuyi8RpSerKx1kE-Th4Jtr8H-xgY",
+  "authDomain": "studio-7284709572-3d188.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "513286659639"
+};
